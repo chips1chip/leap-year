5cc98990ac329cedfa0bcca2ef3a3990287ef74e
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 01:59:21 +0530

    Update LeapYr.java

--- LeapYr.java
+++ LeapYr.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
-
 public class LeapYr {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int year=sc.nextInt();
-        if((year%4==0)&& (year!=100)||(year%400==0)){
-            System.out.println("leap year");
-        }else{
-            System.out.println("not a leap year");
-
-            }
-            sc.close();
-
+        Scanner sc = new Scanner(System.in); // Scanner for input
+        int year = sc.nextInt(); 
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("Leap year"); // If leap year
+        } else {
+            System.out.println("Not a leap year"); // If not leap year
         }
+        sc.close(); 
     }
+}
